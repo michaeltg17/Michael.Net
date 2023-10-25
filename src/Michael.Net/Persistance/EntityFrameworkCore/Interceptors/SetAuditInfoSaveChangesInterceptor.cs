37516a9933464079d93b32fc1a01eaa6874fe9fd
Michael Trullas Garcia <adoser2@hotmail.es>
2023-10-25 20:46:33 +0200

    WIP

---
+++ src/Michael.Net/Persistance/EntityFrameworkCore/Interceptors/SetAuditInfoSaveChangesInterceptor.cs
@@ -0,0 +1,32 @@
+﻿using Michael.Net.Domain;
+using Michael.Net.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Michael.Net.Persistance.EntityFrameworkCore.Interceptors
+{
+    public class SetAuditInfoSaveChangesInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            var entries = eventData.Context!.ChangeTracker.Entries();
+
+            entries.ForEach(e =>
+            {
+                var entity = (IAudited)e.Entity;
+                if (e.State == EntityState.Added)
+                {
+                    entity.CreatedBy = 1;
+                    entity.CreatedOn = DateTime.UtcNow;
+                }
+                else if (e.State == EntityState.Modified)
+                {
+                    entity.ModifiedBy = 1;
+                    entity.ModifiedOn = DateTime.UtcNow;
+                }
+            });
+            
+            return base.SavingChanges(eventData, result);
+        }
+    }
+}
