1c58e2f4a4ef321f15844c977cdf07ffac3d7ac2
Michael Trullas Garcia <adoser2@hotmail.es>
2023-11-04 11:45:06 +0100

    WIP

---
+++ src/Michael.Net/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Michael.Net.Exceptions;
+using System.Text.Json;
+
+namespace Michael.Net.Extensions
+{
+    public static class HttpResponseMessageExtensions
+    {
+        public static async Task<T> FromJson<T>(this HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStreamAsync();
+            var @object = await JsonSerializer.DeserializeAsync<T>(content);
+            return @object ?? throw new MichaelNetException("Deserialization from JSON failed with null value.");
+        }
+    }
+}
