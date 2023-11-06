8bc08006bb3a280804bec2be6d13358e941bf0d4
Michael Trullas Garcia <adoser2@hotmail.es>
2023-11-06 13:51:55 +0100

    WIP

--- src/Michael.Net/Testing/IWebApplicationFactoryAble.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.Hosting;
-
-namespace Michael.Net.Testing
-{
-    public interface IWebApplicationFactoryAble
-    {
-        public static IHostBuilder CreateHostBuilder(string[] args) => throw new NotImplementedException();
-    }
-}
