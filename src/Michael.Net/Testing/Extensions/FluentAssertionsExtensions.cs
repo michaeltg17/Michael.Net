46f894d18726e68c710023c58c2dee7657dce8bd
Michael Trullas Garcia <adoser2@hotmail.es>
2023-12-01 23:43:03 +0100

    WIP

---
+++ src/Michael.Net/Testing/Extensions/FluentAssertionsExtensions.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using FluentAssertions.Equivalency;
+using FluentAssertions.Execution;
+using System.Linq.Expressions;
+
+namespace Michael.Net.Testing.Extensions
+{
+    public static class FluentAssertionsExtensions
+    {
+        public static EquivalencyAssertionOptions<TExpectation> Excluding<TExpectation>(
+            this EquivalencyAssertionOptions<TExpectation> options, params Expression<Func<TExpectation, object>>[] expressions)
+        {
+            foreach (var expression in expressions)
+            {
+                options.Excluding(expression);
+            }
+
+            return options;
+        }
+
+        //public static EquivalencyAssertionOptions<TExpectation> WithPropertyConstraint<TExpectation, TProperty>(
+        //    this EquivalencyAssertionOptions<TExpectation> options, Action<IAssertionContext<TProperty>> action, Expression<TProperty> propertySelector)
+        //{
+        //    options
+        //        .Using<TProperty>(action)
+        //        .When(info => info.Path.EndsWith(propertySelector.Name));
+        //}
+
+        public static EquivalencyAssertionOptions<TSubject> WithPropertyAssertion<TSubject, TProperty>(
+            this EquivalencyAssertionOptions<TSubject> options,
+            Action<IAssertionContext<TProperty>> action,
+            Expression<Func<TSubject, TProperty>> propertySelector)
+        {
+            var propertyName = ((MemberExpression)propertySelector.Body).Member.Name;
+
+            return options
+                .Using<TProperty>(action)
+                .When(info => info.Path.EndsWith(propertyName));
+        }
+    }
+}
---
+++ tests/Michael.Net.Tests/Testing/FluentAssertionsExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Michael.Net.Testing.Extensions;
+using Xunit;
+
+namespace Michael.Net.Tests.Testing
+{
+    public class FluentAssertionsExtensionsTests
+    {
+        [Fact]
+        public void Test()
+        {
+            //Given
+            var class1 = new Class1() { Id = 1, Name = "ThisName" };
+            var class2 = new Class1() { Id = 1, Name = "This Name" };
+
+            //When
+            var assert = () => class1.Should().BeEquivalentTo(class2, o => o.WithPropertyAssertion<Class1, string>( c => c.Subject.Should().Be(c.Expectation)));
+
+            //Then
+            assert.Should().NotThrow();
+        }
+
+        class Class1
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
